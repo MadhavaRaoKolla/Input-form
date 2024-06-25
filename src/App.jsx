9c03a01b89843bbfa60734395fed8e8f9c8f10a3
MadhavaRaoKolla <madhavaraokolla2@gmail.com>
2024-06-25 20:44:35 +0530

    post and delete working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Form from './Form';
 import Item from './Item';
 
 const App = () => {
-  
-const [data, setData] = useState([]); 
+  const [data, setData] = useState([]);  // Data of each entered user - data is set using JSON server
 
-const addData = (newData) => {
-  setData([...data,newData]);
-}
+  const addData = (newData) => { // Input data should be added to data in the server
+    setData([...data, newData]);
+  }
 
-const handleDelete = (firstName) => {
-  const newData = data.filter(item =>  item.firstName !== firstName)
-  setData(newData);
-  // fetch(`http://localhost:7000/data/${firstName}`,{method: 'DELETE'});
-}
+  const handleDelete = (id) => {
+    fetch(`http://localhost:7000/data/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Deleting failed");
+      }
+      setData(data.filter(user => user.id !== id));
+    })
+    .catch(err => console.log(err));
+  }
 
-useEffect( ()=> {
-  fetch('http://localhost:7000/data')
-  .then(res => { return res.json()})
-  .then(data => setData(data))
-  },[])
+  useEffect(() => {
+    fetch('http://localhost:7000/data')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className='container'>
       <header>
         User information page
       </header>
       <main>
-        <Form addData={addData}/>
-        { data && <Item data={data} handleDelete={handleDelete}/>}
+        <Form addData={addData} />
+        {data && <Item data={data} handleDelete={handleDelete} />}
       </main>
     </div>
   );
